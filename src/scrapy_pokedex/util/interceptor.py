a6096859b2cae947c94193b61b74f7679bcc74ce
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-11-17 07:35:13 +0100

    update interceptor.py

--- src/scrapy_pokedex/util/interceptor.py
+++ src/scrapy_pokedex/util/interceptor.py
@@ -1,24 +1,43 @@
-import inspect
+import itertools
 import logging
+from types import FrameType
+from typing import cast
 
 from loguru import logger
 
+__all__ = ["_setup_loguru_logging_intercept", "InterceptHandler"]
+
 
 class InterceptHandler(logging.Handler):
+    """Logs to loguru from Python logging module"""
+
     def emit(self, record: logging.LogRecord) -> None:
-        # Get corresponding Loguru level if it exists.
-        level: str | int
         try:
             level = logger.level(record.levelname).name
         except ValueError:
-            level = record.levelno
+            level = str(record.levelno)
 
-        # Find caller from where originated the logged message.
-        frame, depth = inspect.currentframe(), 0
-        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
-            frame = frame.f_back
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = cast(FrameType, frame.f_back)
             depth += 1
+        logger_with_opts = logger.opt(depth=depth, exception=record.exc_info)
 
-        logger.opt(depth=depth, exception=record.exc_info).log(
-            level, record.getMessage()
-        )
+        try:
+            logger_with_opts.log(level, "{}", record.getMessage())
+        except Exception as e:
+            safe_msg = getattr(record, "msg", None) or str(record)
+            logger_with_opts.warning(
+                "Exception logging the following native logger message: {}, {!r}",
+                safe_msg,
+                e,
+            )
+
+
+def _setup_loguru_logging_intercept(level=logging.DEBUG, modules=()) -> None:
+    logging.basicConfig(handlers=[InterceptHandler()], level=level)
+
+    for logger_name in itertools.chain(("",), modules):
+        mod_logger = logging.getLogger(logger_name)
+        mod_logger.handlers = [InterceptHandler(level=level)]
+        mod_logger.propagate = False
