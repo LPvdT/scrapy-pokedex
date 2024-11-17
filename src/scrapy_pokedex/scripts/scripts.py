bdc16be1a9373902a470fa09dfa2f9b53ed6db58
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-11-17 11:31:39 +0100

    update scripts.py

--- src/scrapy_pokedex/scripts/scripts.py
+++ src/scrapy_pokedex/scripts/scripts.py
@@ -9,27 +9,21 @@ from ..util.constants import COMMANDS
 
 @logger.catch
 def command_factory(command_name: str, *args: str) -> Callable[[], None]:
-    cmd = COMMANDS[command_name]
+    cmd = COMMANDS.get(command_name)
+    if cmd is None:
+        raise ValueError(f"Unknown command: {command_name}")
+
+    cwd = "src" if "cwd" in args else None
 
     @logger.catch
     def execute_command() -> None:
         try:
-            if isinstance(cmd, str):
-                cmd_to_run = shlex.split(cmd)
+            cmds = cmd if isinstance(cmd, list) else [cmd]
+            for command in cmds:
+                cmd_to_run = shlex.split(command)
                 logger.info(f"Running: {cmd_to_run}")
-                subprocess.run(cmd_to_run, cwd="src" if "cwd" in args else None)
-            elif isinstance(cmd, list):
-                for command in cmd:
-                    cmd_to_run = shlex.split(command)
-                    logger.info(f"Running: {cmd_to_run}")
-                    subprocess.run(cmd_to_run, cwd="src" if "cwd" in args else None)
+                subprocess.run(cmd_to_run, cwd=cwd)
         except KeyError:
             raise ValueError(f"Unknown command: {command_name}")
 
     return execute_command
-
-
-scrape = command_factory("scrape", "cwd")
-types = command_factory("gen_types")
-setup_precommit = command_factory("setup_precommit")
-precommit = command_factory("precommit")
