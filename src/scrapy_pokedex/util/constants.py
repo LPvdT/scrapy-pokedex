0c62fb5386ae66f67b6f10e27750ae9139c45194
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-11-17 11:22:55 +0100

    update constants.py

--- src/scrapy_pokedex/util/constants.py
+++ src/scrapy_pokedex/util/constants.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 
+# Paths
 DATA_DIR = Path(__file__).parent / "data"
 DATA_OUTPUT_DIR = DATA_DIR / "output"
 LOG_DIR = DATA_DIR / "logs"
 TYPES_DIR = "typings"
 
+# Spider name
 SPIDER = "pokedex_list"
+
+# Commands for command_factory
+COMMANDS = {
+    "scrape": f"scrapy crawl -O {DATA_OUTPUT_DIR.joinpath('pep').with_suffix('.jsonl')} {SPIDER}",
+    "gen_types": f"stubgen -p scrapy_pokedex -o src/scrapy_pokedex/{TYPES_DIR}",
+    "setup_precommit": ["pre-commit install", "pre-commit autoupdate"],
+    "precommit": "pre-commit run --all-files",
+}
