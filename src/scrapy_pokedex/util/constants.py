0469a2ba88f971ebc174fcaba2e890834ee9d4c3
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-11-17 07:33:24 +0100

    feat: create constants.py

---
+++ src/scrapy_pokedex/util/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+DATA_DIR = Path(__file__).parent / "data"
+DATA_OUTPUT_DIR = DATA_DIR / "output"
+LOG_DIR = DATA_DIR / "logs"
+TYPES_DIR = "typings"
+
+SPIDER = "pokedex_list"
