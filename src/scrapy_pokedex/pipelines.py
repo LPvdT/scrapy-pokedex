17db4804fd7da6b2ed08ecb3bab6b13e37c31791
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-12-09 22:09:00 +0100

    update pipelines.py

--- src/scrapy_pokedex/pipelines.py
+++ src/scrapy_pokedex/pipelines.py
@@ -5,9 +5,43 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter  # noqa E402
+from io import BytesIO
+
+from itemadapter import ItemAdapter
+from scrapy.exporters import JsonItemExporter, JsonLinesItemExporter
+
+from scrapy_pokedex.util.constants import OUTPUT_PATH
+
+from .items import PokedexItem
 
 
 class ScrapyPokedexPipeline:
-    def process_item(self, item, spider):
+    def open_spider(self, _) -> None:
+        # JSONL init
+        self.file_jsonl = BytesIO()
+        self.exporter_jsonl = JsonLinesItemExporter(self.file_jsonl)
+        self.exporter_jsonl.start_exporting()
+
+        # JSON init
+        self.file_json = BytesIO()
+        self.exporter_json = JsonItemExporter(self.file_json)
+        self.exporter_json.start_exporting()
+
+    def close_spider(self, _) -> None:
+        # JSONL finish
+        self.exporter_jsonl.finish_exporting()
+        with open(OUTPUT_PATH.with_suffix(".jsonl"), "wb") as f:
+            f.write(self.file_jsonl.getvalue())
+
+        # JSON finish
+        self.exporter_json.finish_exporting()
+        with open(OUTPUT_PATH.with_suffix(".json"), "wb") as f:
+            f.write(self.file_json.getvalue())
+
+    def process_item(self, item: PokedexItem, _) -> PokedexItem:
+        entry = ItemAdapter(item).asdict()
+
+        self.exporter_jsonl.export_item(entry)
+        self.exporter_json.export_item(entry)
+
         return item
