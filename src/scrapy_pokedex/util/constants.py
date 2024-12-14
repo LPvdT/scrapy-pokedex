7f29392a98bb9443fcc4ee736306dc3e2e9c5058
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-12-14 19:08:52 +0100

    update constants.py

--- src/scrapy_pokedex/util/constants.py
+++ src/scrapy_pokedex/util/constants.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+# Config
+# TODO: Move to YAML config file
+MAX_ROWS: int = 500
+DEFAULT_OUTPUT: bool = False
+
 # Paths
 DATA_DIR = Path(__package__).with_suffix("") / "data"
 DATA_OUTPUT_DIR = DATA_DIR / "output"
@@ -8,18 +13,19 @@ TYPES_DIR = "typings"
 
 # Spider name
 SPIDER = "pokedex_list"
-
-# Max rows to scrape
-MAX_ROWS: int = 1000
+JOB_DIR = DATA_DIR / "crawls" / f"{SPIDER}-1"
 
 # Output
 filename = f"pokedex_{MAX_ROWS}" if MAX_ROWS is not None else "pokedex"
 default_format_ext = ".csv"
 OUTPUT_PATH = DATA_OUTPUT_DIR.joinpath(filename)
+DEFAULT_OUTPUT_URI = (
+    f"-O {DATA_OUTPUT_DIR.joinpath(filename).with_suffix(default_format_ext)}"
+)
 
 # Commands for command_factory
 COMMANDS = {
-    "scrape": f"scrapy crawl -O {DATA_OUTPUT_DIR.joinpath(filename).with_suffix(default_format_ext)} {SPIDER}",
+    "scrape": f"scrapy crawl {DEFAULT_OUTPUT if DEFAULT_OUTPUT else ''} {SPIDER} -s JOBDIR={JOB_DIR}",
     "gen_types": f"stubgen -p scrapy_pokedex -o src/scrapy_pokedex/{TYPES_DIR}",
     "setup_precommit": ["pre-commit install", "pre-commit autoupdate"],
     "precommit": "pre-commit run --all-files",
