3cc9dceda7e9166bb16583b4e981d125f513a2a2
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-12-14 22:08:30 +0100

    update fs.py

--- src/scrapy_pokedex/util/fs.py
+++ src/scrapy_pokedex/util/fs.py
@@ -7,24 +7,25 @@ from .constants import DATA_DIR, DATA_OUTPUT_DIR, LOG_DIR
 __all__ = ["_setup_filesystem"]
 
 
-def _setup_filesystem() -> None:
+def _setup_filesystem(enable_debug: bool = False) -> None:
     logger.add(
         LOG_DIR.joinpath("info_log").with_suffix(".log"),
         level=logging.INFO,
         rotation="1 week",
     )
 
-    logger.add(
-        LOG_DIR.joinpath("debug_log").with_suffix(".log"),
-        level=logging.DEBUG,
-        rotation="1 day",
-    )
+    if enable_debug:
+        logger.add(
+            LOG_DIR.joinpath("debug_log").with_suffix(".log"),
+            level=logging.DEBUG,
+            rotation="1 day",
+        )
 
     for dir in [DATA_OUTPUT_DIR, LOG_DIR]:
         if not dir.exists():
             dir.mkdir(parents=True)
 
             logger.warning("Filesystem not set up. Setting up filesystem:")
-            logger.debug(f"DATA_DIR: {DATA_DIR}")
-            logger.debug(f"DATA_OUTPUT_DIR: {DATA_OUTPUT_DIR}")
-            logger.debug(f"LOG_DIR: {LOG_DIR}")
+            logger.info(f"DATA_DIR: {DATA_DIR}")
+            logger.info(f"DATA_OUTPUT_DIR: {DATA_OUTPUT_DIR}")
+            logger.info(f"LOG_DIR: {LOG_DIR}")
