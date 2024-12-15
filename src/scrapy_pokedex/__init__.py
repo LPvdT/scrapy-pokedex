65e5da96e4c285fc376a811f74f421cd0a584673
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-12-15 06:03:26 +0100

    update src/scrapy_pokedex/__init__.py

--- src/scrapy_pokedex/__init__.py
+++ src/scrapy_pokedex/__init__.py
@@ -1,6 +1,11 @@
-from .util.constants import ENABLE_DEBUG
+import logging
+
+from .settings import ENABLE_DEBUG
 from .util.fs import _setup_filesystem
 from .util.interceptor import _setup_loguru_logging_intercept
 
-_setup_loguru_logging_intercept(modules=("scrapy_pokedex",))
+_setup_loguru_logging_intercept(
+    level=logging.INFO if not ENABLE_DEBUG else logging.DEBUG,
+    modules=("scrapy_pokedex",),
+)
 _setup_filesystem(ENABLE_DEBUG)
