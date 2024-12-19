a80d51f9c5a28ac7887e1b319b18f1630ec37bfc
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-12-19 21:19:04 +0100

    update pipelines.py: implement batch writing

--- src/scrapy_pokedex/pipelines.py
+++ src/scrapy_pokedex/pipelines.py
@@ -14,45 +14,77 @@ from scrapy.exporters import BaseItemExporter, JsonItemExporter, JsonLinesItemEx
 from scrapy_pokedex.util.constants import OUTPUT_PATH
 
 from .items import PokedexItem
+from .settings import BATCH_SIZE
 
 T = TypeVar("T", bound=BaseItemExporter)
 
 
 class ScrapyPokedexPipeline:
-    def open_spider(self, _) -> None:
-        def initialize_exporter(file: BytesIO, exporter_class: Type[T]) -> T:
-            exporter = exporter_class(
-                file=file, export_empty_fields=True, indent=2, encoding="utf-8"
-            )
-            exporter.start_exporting()
+    def __init__(self) -> None:
+        self.buffer: list[PokedexItem] = []
+        self.batch_size = BATCH_SIZE if BATCH_SIZE is not None else 250
+        self.batch_count: int = 1
 
-            return exporter
+        self.file_jsonl: BytesIO
+        self.file_json: BytesIO
 
-        # JSONL init
+    def _initialize_exporter(self, file: BytesIO, exporter_class: Type[T]) -> T:
+        exporter = exporter_class(
+            file=file, export_empty_fields=True, indent=2, encoding="utf-8"
+        )
+        exporter.start_exporting()
+
+        return exporter
+
+    def _flush_buffer(self) -> None:
+        # JSONL exporter
         self.file_jsonl = BytesIO()
-        self.exporter_jsonl = initialize_exporter(
+        self.exporter_jsonl = self._initialize_exporter(
             self.file_jsonl, JsonLinesItemExporter
         )
 
-        # JSON init
+        # JSON exporter
         self.file_json = BytesIO()
-        self.exporter_json = initialize_exporter(self.file_json, JsonItemExporter)
+        self.exporter_json = self._initialize_exporter(self.file_json, JsonItemExporter)
 
-    def close_spider(self, _) -> None:
+        # Export items
+        for item in self.buffer:
+            self.exporter_jsonl.export_item(item)
+            self.exporter_json.export_item(item)
+
         # JSONL finish
         self.exporter_jsonl.finish_exporting()
-        with open(OUTPUT_PATH.with_suffix(".jsonl"), "wb") as f:
+
+        output_jsonl = OUTPUT_PATH.with_name(
+            f"{OUTPUT_PATH.name}_{self.batch_size}_{self.batch_count}"
+        ).with_suffix(".jsonl")
+        with open(output_jsonl, "wb") as f:
             f.write(self.file_jsonl.getvalue())
 
         # JSON finish
         self.exporter_json.finish_exporting()
-        with open(OUTPUT_PATH.with_suffix(".json"), "wb") as f:
+        output_json = OUTPUT_PATH.with_name(
+            f"{OUTPUT_PATH.name}_{self.batch_size}_{self.batch_count}"
+        ).with_suffix(".json")
+        with open(output_json, "wb") as f:
             f.write(self.file_json.getvalue())
+
+        # Clear buffer
+        self.buffer = []
+        self.batch_count += 1
 
     def process_item(self, item: PokedexItem, _) -> PokedexItem:
         entry = ItemAdapter(item).item
+        self.buffer.append(entry)
 
-        self.exporter_jsonl.export_item(entry)
-        self.exporter_json.export_item(entry)
+        if len(self.buffer) >= self.batch_size:
+            self._flush_buffer()
 
         return item
+
+    def open_spider(self, _) -> None:
+        pass
+
+    def close_spider(self, _) -> None:
+        if len(self.buffer) > 0:
+            self._flush_buffer()
